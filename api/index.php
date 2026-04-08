9748bdbce8b9937e2e6b2703fe2966d0a080af1a
ayawazzani <2005120400095@ofppt-edu.ma>
2026-04-08 19:38:49 +0100

    ll

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
+// 1. إعداد مسارات التخزين المؤقت لتناسب بيئة Vercel
+$storagePath = '/tmp/storage/framework';
+$folders = ['/views', '/sessions', '/cache'];
+
+foreach ($folders as $folder) {
+    if (!is_dir($storagePath . $folder)) {
+        mkdir($storagePath . $folder, 0755, true);
+    }
+}
+
+// 2. إخبار Laravel باستخدام المسارات الجديدة
+putenv("VIEW_COMPILED_PATH=$storagePath/views");
+putenv("SESSION_DRIVER=file");
+putenv("SESSION_PATH=$storagePath/sessions");
+
+// استكمال تشغيل Laravel كما هو موجود لديك سابقاً
 require __DIR__ . '/../public/index.php';
