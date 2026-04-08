6265cd4f41a076b79e3d1cfddfe3807f7531e354
ayawazzani <2005120400095@ofppt-edu.ma>
2026-04-08 21:25:25 +0100

    llll

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,5 +16,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(200);
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
+
+
+
+
