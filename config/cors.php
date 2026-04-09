d32a0cf744d23a64148a4f6062710a3006f5ce00
ayawazzani <2005120400095@ofppt-edu.ma>
2026-04-09 13:37:37 +0100

    mnb

--- config/cors.php
+++ config/cors.php
@@ -3,19 +3,20 @@
 return [
 
     'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-        'voyages/search-json',
-    ],
+    'api/*',
+    'sanctum/csrf-cookie',
+    'voyages/search-json',
+],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
-    ],
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
+    'https://oncf-react-tau.vercel.app',
+],
 
     'allowed_origins_patterns' => [],
 
